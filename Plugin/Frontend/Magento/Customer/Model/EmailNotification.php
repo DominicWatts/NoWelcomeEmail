2e2ee07dc1bc2305478bf73a5c55870fbc8cf93b
Dominic Watts <dominic@pixie.agency>
2022-07-21 17:43:22 +0100

    Fixes

--- Plugin/Frontend/Magento/Customer/Model/EmailNotification.php
+++ Plugin/Frontend/Magento/Customer/Model/EmailNotification.php
@@ -14,14 +14,19 @@ class EmailNotification
     const XML_IS_DISABLED = 'nowelcomeemail/options/is_disabled';
 
     /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $_scopeConfig;
+
+    /**
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
      */
     public function __construct(
         ScopeConfigInterface $scopeConfig
-    ) { 
+    ) {
         $this->_scopeConfig = $scopeConfig;
     }
-    
+
     public function aroundNewAccount(
         \Magento\Customer\Model\EmailNotification $subject,
         \Closure $proceed,
@@ -31,7 +36,7 @@ class EmailNotification
         $storeId = 0,
         $sendemailStoreId = null
     ) {
-        if(!$this->_scopeConfig->isSetFlag(self::XML_IS_DISABLED, ScopeInterface::SCOPE_STORE)) {
+        if (!$this->_scopeConfig->isSetFlag(self::XML_IS_DISABLED, ScopeInterface::SCOPE_STORE)) {
             $result = $proceed($customer, $type, $backUrl, $storeId, $sendemailStoreId);
             return $result;
         }
